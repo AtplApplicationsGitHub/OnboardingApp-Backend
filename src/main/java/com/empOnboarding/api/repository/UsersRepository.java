a4facabec2d4f26ffa14a4f2b1842f4697794881
monica07092002 <monica.surendar@adventsys.co>
2025-08-13 14:46:39 +0530

    Constant file commit

--- src/main/java/com/empOnboarding/api/repository/UsersRepository.java
+++ src/main/java/com/empOnboarding/api/repository/UsersRepository.java
@@ -12,34 +12,26 @@ import org.springframework.data.repository.query.Param;
 import com.empOnboarding.api.entity.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
-	
+
 	Optional<Users> findByNameOrEmail(String username, String email);
-	
-	@Query(value ="SELECT u.* FROM users u \r\n" + 
-			"WHERE u.active_flag =:y AND u.delete_flag =:n", nativeQuery = true)
+
+	@Query(value = "SELECT u.* FROM users u \r\n" + "WHERE u.active_flag =:y AND u.delete_flag =:n", nativeQuery = true)
 	List<Users> loadAllActiveUsers(String y, String n);
 
 	Page<Users> findAllByRoleOrderByCreatedTimeDesc(String role, Pageable page);
 
 	Page<Users> findAllByOrderByCreatedTimeDesc(Pageable pageable);
-	
-	@Query(value = "SELECT u.* FROM users u " +
-		            "WHERE u.active_flag = :activeFlag " +
-		            "AND (u.name ILIKE CONCAT('%', :keyword, '%') " +
-		            "OR u.email ILIKE CONCAT('%', :keyword, '%') " +
-		            "OR u.role ILIKE CONCAT('%', :keyword, '%')) " +
-		            "ORDER BY u.created_time DESC",
-		    countQuery = "SELECT count(*) FROM users u " +
-		                 "WHERE u.active_flag = :activeFlag " +
-		                 "AND (u.name ILIKE CONCAT('%', :keyword, '%') " +
-		                 "OR u.email ILIKE CONCAT('%', :keyword, '%') " +
-		                 "OR u.role ILIKE CONCAT('%', :keyword, '%'))",
-		    nativeQuery = true)
-		Page<Users> findAllBySearch(@Param("keyword") String keyword,
-		                            @Param("activeFlag") String activeFlag,
-		                            Pageable pageable);
+
+	@Query(value = "SELECT u.* FROM users u " + "WHERE u.active_flag = :activeFlag "
+			+ "AND (u.name ILIKE CONCAT('%', :keyword, '%') " + "OR u.email ILIKE CONCAT('%', :keyword, '%') "
+			+ "OR u.role ILIKE CONCAT('%', :keyword, '%')) "
+			+ "ORDER BY u.created_time DESC", countQuery = "SELECT count(*) FROM users u "
+					+ "WHERE u.active_flag = :activeFlag " + "AND (u.name ILIKE CONCAT('%', :keyword, '%') "
+					+ "OR u.email ILIKE CONCAT('%', :keyword, '%') "
+					+ "OR u.role ILIKE CONCAT('%', :keyword, '%'))", nativeQuery = true)
+	Page<Users> findAllBySearch(@Param("keyword") String keyword, @Param("activeFlag") String activeFlag,
+			Pageable pageable);
 
 	List<Users> findAllByRole(String string);
 
-	
 }
