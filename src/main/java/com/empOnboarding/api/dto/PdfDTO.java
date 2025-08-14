d37d6b801b905c97addbbad934e8daf9af149778
monica07092002 <monica.surendar@adventsys.co>
2025-08-14 09:34:06 +0530

    PDF DTO COMMIT CHECK

---
+++ src/main/java/com/empOnboarding/api/dto/PdfDTO.java
@@ -0,0 +1,20 @@
+package com.empOnboarding.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Generated
+public class PdfDTO {
+	
+	byte[] pdf;
+	
+	String fileName="";
+
+}
