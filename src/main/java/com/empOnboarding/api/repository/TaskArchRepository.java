c742979936724a0ab54f4c34ccc9e31d107f371d
monica07092002 <monica.surendar@adventsys.co>
2025-09-29 23:36:45 +0530

    Employee  Commit

--- src/main/java/com/empOnboarding/api/repository/TaskArchRepository.java
+++ src/main/java/com/empOnboarding/api/repository/TaskArchRepository.java
@@ -3,4 +3,7 @@ package com.empOnboarding.api.repository;
 import com.empOnboarding.api.entity.TaskArch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskArchRepository extends JpaRepository<TaskArch, String> {}
+public interface TaskArchRepository extends JpaRepository<TaskArch, String> {
+
+    TaskArch findTopByIdStartingWithOrderByIdDesc(String prefix);
+}
